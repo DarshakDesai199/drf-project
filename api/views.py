c58016e10a951c92b48c41c63b2bd79ece464f4b
Darshak Desai <darshakdesai2002@gmail.com>
2025-08-25 21:54:57 +0530

    Added mixin and generics view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 # from django.http import JsonResponse
 # from django.shortcuts import render
+from logging import exception
 from operator import truediv
 from django.core.serializers import serialize
 from rest_framework.response import Response
@@ -11,6 +12,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework import mixins, generics
 
 
 # def studentsView(request):
@@ -74,3 +77,56 @@ class Employees(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EmployeeDetail(APIView):
+    def get_objects(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+
+        except Employee.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        serializer = EmployeeSerializer(self.get_objects(pk))
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        serializer = EmployeeSerializer(self.get_objects(pk), data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        employee = self.get_objects(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# MixIn Example
+class EmployeesUsingMixIn(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class EmployeesUsingMixInDetails(mixins.RetrieveModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin,
+                                 generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
